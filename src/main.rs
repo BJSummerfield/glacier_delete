4edeb055a5b832301be7d95e97b45f1f91e7dc62
BJSummerfield <brianjsummerfield@gmail.com>
2025-09-18 14:58:06 -0500

    update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,14 @@
+mod inventory;
+use anyhow::Result;
+use dotenvy::dotenv;
+use inventory::{DeletedInventory, Inventory};
+
 #[tokio::main]
-async fn main() {
-    println!("Hello, world!");
+async fn main() -> Result<()> {
+    dotenv().ok();
+    let mut inventory = Inventory::new().await?;
+    let deleted_inventory = DeletedInventory::new().await?;
+    inventory.filter_deleted(&deleted_inventory.ids);
+    println!("{:?}", inventory.archive_list.len());
+    Ok(())
 }
